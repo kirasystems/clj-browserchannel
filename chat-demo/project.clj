94818d11fff242c297839b3e9eb9aadec069358b
Gijs Stuurman <gijsstuurman@gmail.com>
2012-04-16 21:10:57 +0200

    fix project.clj for AsyncContext signing warning

--- chat-demo/project.clj
+++ chat-demo/project.clj
@@ -1,7 +1,7 @@
 (defproject chat-demo "0.0.1"
   :description "Example for using BrowserChannel and a client side with ClojureScript"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.1.0-SNAPSHOT"]
+                 [ring/ring-core "1.1.0-SNAPSHOT" :exclusions [javax.servlet/servlet-api]]
                  [org.clojure/clojurescript "0.0-1011" :exclusions [org.clojure/google-closure-library]]
                  [net.thegeez/google-closure-library "0.0-1698"]
                  [net.thegeez/clj-browserchannel-server "0.0.1"]
