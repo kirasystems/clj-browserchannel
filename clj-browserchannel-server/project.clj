fea3cbc12956404a0387f8bab59ec622566fa192
Gijs Stuurman <gijsstuurman@gmail.com>
2012-05-11 22:29:27 +0200

    bump server version number, includes pull request #3

--- clj-browserchannel-server/project.clj
+++ clj-browserchannel-server/project.clj
@@ -1,4 +1,4 @@
-(defproject net.thegeez/clj-browserchannel-server "0.0.2"
+(defproject net.thegeez/clj-browserchannel-server "0.0.3"
   :description "BrowserChannel server implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.1.0-beta3"]
