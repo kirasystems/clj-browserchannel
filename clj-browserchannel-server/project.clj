c01759f5f638cf83eaf3b3abb849f72eaf22ad08
Gijs Stuurman <gijsstuurman@gmail.com>
2012-06-05 17:29:23 +0200

    new clj-browserchannel-server version

--- clj-browserchannel-server/project.clj
+++ clj-browserchannel-server/project.clj
@@ -1,4 +1,4 @@
-(defproject net.thegeez/clj-browserchannel-server "0.0.4"
+(defproject net.thegeez/clj-browserchannel-server "0.0.5"
   :description "BrowserChannel server implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.1.0-beta3"]
