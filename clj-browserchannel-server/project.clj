4331e171eeaa8d659a021b2cae98472f1cc729a9
Dave Della Costa <ddellacosta@gmail.com>
2014-04-08 13:03:48 +0900

    upgrades data.json to 0.2.4. Still using deprecated function json-str, will port at a later date.

--- clj-browserchannel-server/project.clj
+++ clj-browserchannel-server/project.clj
@@ -2,4 +2,4 @@
   :description "BrowserChannel server implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.1.0-beta3"]
-                 [org.clojure/data.json "0.1.3"]])
+                 [org.clojure/data.json "0.2.4"]]) ; still using deprecated json-str, need to port to write-str (?)
