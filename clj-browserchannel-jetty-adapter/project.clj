11f80ef06ccecd9b87dee2172a420e2b99d887f6
Dave Della Costa <ddellacosta@gmail.com>
2014-09-16 14:00:31 +0900

    bump ring to 1.2.2

--- clj-browserchannel-jetty-adapter/project.clj
+++ clj-browserchannel-jetty-adapter/project.clj
@@ -1,8 +1,8 @@
-(defproject net.thegeez/clj-browserchannel-jetty-adapter "0.0.1"
+(defproject net.thegeez/clj-browserchannel-jetty-adapter "0.0.2"
   :description "Jetty async adapter for BrowserChannel"
   :url ""
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.2.0"]
+                 [ring/ring-core "1.2.2"]
                  [ring/ring-servlet "1.2.0" :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "8.1.13.v20130916"];; includes ssl
                  [net.thegeez/clj-browserchannel-server "0.0.5"]
--- clj-browserchannel-server/project.clj
+++ clj-browserchannel-server/project.clj
@@ -1,5 +1,5 @@
-(defproject net.thegeez/clj-browserchannel-server "0.0.5"
+(defproject net.thegeez/clj-browserchannel-server "0.0.6"
   :description "BrowserChannel server implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.1.0-beta3"]
+                 [ring/ring-core "1.2.2"]
                  [org.clojure/data.json "0.2.4"]]) ; still using deprecated json-str, need to port to write-str (?)
