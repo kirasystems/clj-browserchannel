c196db269e2aecef21900383d5698ac23ed2a26f
Dave Della Costa <ddellacosta@gmail.com>
2014-09-16 14:00:31 +0900

    bump ring to 1.2.2

--- clj-browserchannel-jetty-adapter/project.clj
+++ clj-browserchannel-jetty-adapter/project.clj
@@ -2,7 +2,7 @@
   :description "Jetty async adapter for BrowserChannel"
   :url ""
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.2.0"]
+                 [ring/ring-core "1.2.2"]
                  [ring/ring-servlet "1.2.0" :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "8.1.13.v20130916"];; includes ssl
                  [net.thegeez/clj-browserchannel-server "0.0.5"]
