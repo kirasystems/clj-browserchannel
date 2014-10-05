5a2f8b97fc7ebbf8a810cdac8752856420552dc7
Alexander K. Hudek <alexander@hudek.org>
2014-10-04 22:41:51 -0400

    Bump ring-servlet.

--- clj-browserchannel-jetty-adapter/project.clj
+++ clj-browserchannel-jetty-adapter/project.clj
@@ -3,7 +3,7 @@
   :url ""
   :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
                  [ring/ring-core "1.3.1"]
-                 [ring/ring-servlet "1.3.0" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-servlet "1.3.1"]
                  [org.eclipse.jetty/jetty-server "8.1.16.v20140903"];; includes ssl
                  [net.thegeez/clj-browserchannel-server "0.0.7-SNAPSHOT"]
                  ])
