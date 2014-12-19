ce554b8f6392a92b40a150ef7cf220e68f8f4e82
gered <gered@blarg.ca>
2014-12-19 16:58:39 -0500

    pass incoming browserchannel message request maps to listeners

--- clj-browserchannel-server/project.clj
+++ clj-browserchannel-server/project.clj
@@ -1,4 +1,4 @@
-(defproject net.thegeez/clj-browserchannel-server "0.0.9"
+(defproject net.thegeez/clj-browserchannel-server "0.1.0"
   :description "BrowserChannel server implementation in Clojure"
   :dependencies [[ring/ring-core "1.3.1"]
                  [org.clojure/data.json "0.2.5"]]
